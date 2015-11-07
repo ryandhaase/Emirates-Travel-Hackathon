e80704a7a763b4780521c602e4dca92cd4a952ca
Ryan Haase <ryandhaase@gmail.com>
2015-11-07 12:27:11 -0800

    Events:
    * Created initial events model
    * Created map partial
    * TODO: Add columns to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107201603) do
+ActiveRecord::Schema.define(version: 20151107202044) do
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "events", ["user_id"], name: "index_events_on_user_id"
 
   create_table "landings", force: :cascade do |t|
     t.string   "home"
